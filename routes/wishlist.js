e2aea6be5864e8119dd0356c27afac895de39056
alamin <talha123.ah.ah@gmail.com>
2023-01-13 01:21:42 +0600

    initial commit

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -5,16 +5,21 @@ const ExpressError = require('../utils/ExpressError');
 const Ground = require('../models/grounds');
 const Wishlist = require('../models/wishlist');
 const { route } = require('./books');
+const { isLoggedIn } = require('../midddleware');
 
 
-router.post('/grounds/:id/wishlist', async(req, res) => {
+router.post('/grounds/:id/wishlist', isLoggedIn, async(req, res) => {
     const ground = await Ground.findById(req.params.id);
     const wishlist = new Wishlist(req.body.wishlist);
+    wishlist.author = req.user._id;
     ground.wishlist.push(wishlist);
     await wishlist.save();
     await ground.save();
+    req.flash('success', 'Add to wishlist');
     res.redirect('/wishlist');
 })
+
+
 
 router.get('/wishlist', async(req, res) => {
     const ground = await Ground.find({})
@@ -28,5 +33,7 @@ router.delete('/wishlist/:id', catchAsync(async(req, res) => {
     req.flash('success', 'Successfully deleted ground')
     res.redirect('/wishlist');
 }))
+
+
 
 module.exports = router;
